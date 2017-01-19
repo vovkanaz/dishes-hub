bd6db34937dfece94c017b0d781b3a17a27ff036
vova <vova@robots@masterofcode.com>
2017-01-19 12:40:19 +0200

    Add Recipes and Category controllers and models

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
-
+  get 'category', to: 'category#index'
+  get 'recipe', to: 'recipe#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
