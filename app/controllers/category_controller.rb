189cf66193f27fb2c8fb63c4159f1b4658b73e95
vovkanaz <vovkanaz@gmail.com>
2017-01-23 20:58:24 +0200

    Add blog structure

--- app/controllers/category_controller.rb
+++
@@ -1,6 +0,0 @@
-class CategoryController < ApplicationController
-  def index
-    @categories = Category.find(1)
-    @category_recipes = @categories.recipes
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'home#index'
-  get 'categories', to: 'category#index'
+  #get 'categories', to: 'category#index'
+  #get 'categories/:id', to: 'category#show'
+  resources :categories
+  resources :recipes
   get 'recipes', to: 'recipe#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
